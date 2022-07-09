ab8d1a95e16275205e5877a40547195f66d6cc65
jayden000106 <cars0106@icloud.com>
2022-07-09 11:33:51 +0900

    [FEAT] 컨텐츠 세부 화면 뷰 추가

---
+++ NeflixCloneCoding/NeflixCloneCoding/Detail/ContentDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentDetailView.swift
+//  NeflixCloneCoding
+//
+//  Created by 정지혁 on 2022/07/09.
+//
+
+import SwiftUI
+
+struct ContentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentDetailView()
+    }
+}
