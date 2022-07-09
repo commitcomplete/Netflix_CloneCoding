e752f6088e8e4f6b43ef7664b0e4454a8181f813
jayden000106 <cars0106@icloud.com>
2022-07-10 03:10:15 +0900

    [FEAT] #2 컨텐츠 세부 정보 텍스트 구현
    - 더미 데이터를 텍스트에 넣어서 보여줌(Fix Needed)

--- NeflixCloneCoding/NeflixCloneCoding/Detail/ContentDetailView.swift
+++ NeflixCloneCoding/NeflixCloneCoding/Detail/ContentDetailView.swift
@@ -21,6 +21,37 @@ struct ContentDetailView: View {
                     .padding(.horizontal, 8)
                     .padding(.vertical, 10)
                 
+                Text("학업과 재정 측면에서 어려움을 겪고 있는 학생. 학교 경비원에게 수학을 가르쳐 달라고 요청한다. 그리고 성적보다 훨씬 더 많은 것을 얻게 되는데.")
+                    .font(.system(size: 12))
+                    .padding(.horizontal, 8)
+                
+                HStack(spacing: 0) {
+                    Text("출연: ")
+                        .font(.system(size: 10))
+                    
+                    Text("최민식, 김동회, 박병은 ...")
+                        .font(.system(size: 10))
+                    
+                    Button(action: {}, label: {
+                        Text("더보기")
+                            .font(.system(size: 10))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.secondary)
+                    })
+                }
+                .padding(.horizontal, 8)
+                .padding(.top, 10)
+                
+                HStack(spacing: 0) {
+                    Text("감독: ")
+                        .font(.system(size: 10))
+                    
+                    Text("박동훈")
+                        .font(.system(size: 10))
+                }
+                .padding(.horizontal, 8)
+                .padding(.top, 4)
+                
                 Spacer()
             }
         }
