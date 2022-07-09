905f38745d8a0216c1f543457624f289c42b22f5
jayden000106 <cars0106@icloud.com>
2022-07-10 01:53:03 +0900

    [FEAT] 상단 예고편 또는 스틸이미지를 위한 사각형 구현
    - 검은 배경, 다크 테마 적용
    - 상단 두 모서리만 둥근 사각형 구현

--- NeflixCloneCoding/NeflixCloneCoding/Detail/ContentDetailView.swift
+++ NeflixCloneCoding/NeflixCloneCoding/Detail/ContentDetailView.swift
@@ -9,12 +9,38 @@ import SwiftUI
 
 struct ContentDetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack(spacing: 0) {
+                TopRoundedRectangle(radius: 13, height: 220)
+                
+                Spacer()
+            }
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
 struct ContentDetailView_Previews: PreviewProvider {
     static var previews: some View {
         ContentDetailView()
+            .previewDevice("iPhone 13 Pro")
+    }
+}
+
+// 상단 모서리만 둥근 모서리로 만드는 코드
+// https://stackoverflow.com/questions/56760335/round-specific-corners-swiftui
+struct TopRoundedRectangle: View {
+    var radius: CGFloat;
+    var height: CGFloat;
+    var body: some View {
+        Rectangle()
+            .frame(height: height)
+            .foregroundColor(.orange)
+            .padding(.bottom, radius)
+            .cornerRadius(radius)
+            .padding(.bottom, -radius)
     }
 }
