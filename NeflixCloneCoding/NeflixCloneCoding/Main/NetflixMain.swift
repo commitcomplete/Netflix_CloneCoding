560d0db4878235354fc2f56fa25099f19490b359
commitcomplete <72395020+commitcomplete@users.noreply.github.com>
2022-07-10 14:28:45 +0900

    [FEAT] 하단 탭바 추가
    하단에 존재하는 탭바 추가
    탭바 클릭시 다른 뷰로 이동하는 부분까지
    구현

--- NeflixCloneCoding/NeflixCloneCoding/Main/NetflixMain.swift
+++ NeflixCloneCoding/NeflixCloneCoding/Main/NetflixMain.swift
@@ -9,33 +9,61 @@ import SwiftUI
 
 struct NetflixMain: View {
     @State var iscategoryClicked = false
+    init(){
+        UITabBar.appearance().barTintColor = .black
+    }
     var body: some View {
-        ZStack{
-            Color.black
-            ScrollView{
-                ZStack{
-                    //main content 들어가야되는 부분
-                    VStack{
-                        Image("MainTitle")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: UIScreen.main.bounds.width , height: UIScreen.main.bounds.height * 0.5, alignment: .center)
+        
+        
+        TabView{
+            ZStack{
+                Color.black
+                ScrollView{
+                    ZStack{
+                        //main content 들어가야되는 부분
+                        VStack{
+                            Image("MainTitle")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: UIScreen.main.bounds.width , height: UIScreen.main.bounds.height * 0.5, alignment: .center)
+                            
+                        }
+                        //상단바를 위한 스택
+                        VStack{
+                            TopContent(iscategoryClicked: $iscategoryClicked)
+                            Spacer()
+                        }
                         
                     }
-                    //상단바를 위한 스택
-                    VStack{
-                        TopContent(iscategoryClicked: $iscategoryClicked)
-                        Spacer()
-                    }
-                    
                 }
+                if iscategoryClicked{
+                    CategoryselectionView(iscategoryClicked: $iscategoryClicked)
+                }
+                
             }
-            if iscategoryClicked{
-                CategoryselectionView(iscategoryClicked: $iscategoryClicked)
+            .ignoresSafeArea()
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("홈")
             }
-            
+            ZStack{
+                Color.black.ignoresSafeArea()
+            }.tabItem{Image(systemName: "newspaper")
+                Text("New & Hot")}
+            ZStack{
+                Color.black.ignoresSafeArea()
+            }.tabItem{Image(systemName: "magnifyingglass")
+                Text("검색")}
+            ZStack{
+                Color.black.ignoresSafeArea()
+            }.tabItem{Image(systemName: "square.and.arrow.down")
+                    .foregroundColor(Color.white)
+                Text("저장한 컨텐츠 목록")
+                    .foregroundColor(.white)
+            }
         }
-        .ignoresSafeArea()
+        .accentColor(.white)
+        
     }
 }
 
