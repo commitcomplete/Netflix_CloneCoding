a3fe011e79aabc1aeda24d094543fc99b11e6aef
jayden000106 <cars0106@icloud.com>
2022-07-10 12:00:02 +0900

    [FEAT] #2 하단 세부 텍스트 구현
    - 스크롤 뷰 사용
    - Text에 리터럴을 직접 사용(Fix Needed)
    - 더보기 버튼 구현

--- NeflixCloneCoding/NeflixCloneCoding/Detail/ContentDetailView.swift
+++ NeflixCloneCoding/NeflixCloneCoding/Detail/ContentDetailView.swift
@@ -13,46 +13,58 @@ struct ContentDetailView: View {
             Color.black
                 .ignoresSafeArea()
             
-            VStack(alignment: .leading, spacing: 0) {
-                TopRoundedRectangle(radius: 13, height: 220)
-                
-                Text("이상한 나라의 수학자")
-                    .font(.system(size: 16))
-                    .padding(.horizontal, 8)
-                    .padding(.vertical, 10)
-                
-                Text("학업과 재정 측면에서 어려움을 겪고 있는 학생. 학교 경비원에게 수학을 가르쳐 달라고 요청한다. 그리고 성적보다 훨씬 더 많은 것을 얻게 되는데.")
-                    .font(.system(size: 12))
-                    .padding(.horizontal, 8)
-                
-                HStack(spacing: 0) {
-                    Text("출연: ")
-                        .font(.system(size: 10))
+            ScrollView {
+                VStack(alignment: .leading, spacing: 0) {
+                    TopRoundedRectangle(radius: 13, height: 220)
                     
-                    Text("최민식, 김동회, 박병은 ...")
-                        .font(.system(size: 10))
+                    Text("이상한 나라의 수학자")
+                        .font(.system(size: 16))
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 10)
                     
-                    Button(action: {}, label: {
-                        Text("더보기")
+                    Text("학업과 재정 측면에서 어려움을 겪고 있는 학생. 학교 경비원에게 수학을 가르쳐 달라고 요청한다. 그리고 성적보다 훨씬 더 많은 것을 얻게 되는데.")
+                        .font(.system(size: 12))
+                        .padding(.horizontal, 8)
+                    
+                    HStack(spacing: 0) {
+                        Text("출연: ")
                             .font(.system(size: 10))
-                            .fontWeight(.semibold)
-                            .foregroundColor(.secondary)
-                    })
-                }
-                .padding(.horizontal, 8)
-                .padding(.top, 10)
-                
-                HStack(spacing: 0) {
-                    Text("감독: ")
-                        .font(.system(size: 10))
+                        
+                        Text("최민식, 김동회, 박병은 ...")
+                            .font(.system(size: 10))
+                        
+                        Button(action: {}, label: {
+                            Text("더보기")
+                                .font(.system(size: 10))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.secondary)
+                        })
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.top, 10)
                     
-                    Text("박동훈")
-                        .font(.system(size: 10))
+                    HStack(spacing: 0) {
+                        Text("감독: ")
+                            .font(.system(size: 10))
+                        
+                        Text("박동훈")
+                            .font(.system(size: 10))
+                    }
+                    .padding(.horizontal, 8)
+                    .padding(.top, 4)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Rectangle()
+                            .fill(.red)
+                            .frame(maxWidth: 97, maxHeight: 4)
+                        
+                        Text("함께 시청된 컨텐츠")
+                            .font(.system(size: 13, weight: .semibold))
+                            .padding(.top, 10)
+                    }
+                    .padding(.horizontal, 12)
+                    .padding(.top, 10)
                 }
-                .padding(.horizontal, 8)
-                .padding(.top, 4)
-                
-                Spacer()
             }
         }
         .preferredColorScheme(.dark)
